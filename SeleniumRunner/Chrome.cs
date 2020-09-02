2cf9f9a9fe60d219f627c1d290ba870b8f4dec0a
Johnny Oshika <johnnyoshika@gmail.com>
2020-09-02 15:43:25 -0700

    Add Selenium test using Chrome

---
+++ SeleniumRunner/Chrome.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using static System.Threading.Thread;
+
+namespace SeleniumRunner
+{
+    public class Chrome
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test_Google_Search()
+        {
+            driver.Url = "https://www.google.com";
+
+            Sleep(2000);
+
+            var input = driver.FindElement(By.CssSelector("[name='q']"));
+
+            input.SendKeys("Vancouver Jobs");
+
+            var button = driver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]"));
+
+            button.Click();
+
+            Sleep(6000);
+
+            Console.WriteLine("Test Passed");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
